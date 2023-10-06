8b2826bf9be59030d7046e2c6a31755112866663
ShubBhavi <132920127+ShubBhavi@users.noreply.github.com>
2023-10-06 21:22:40 +0530

    Add files via upload

---
+++ day02/lab02.py
@@ -0,0 +1,23 @@
+print("shubham","bhavi",sep="  ",end="\t")
+print("python")
+
+#single line comment
+
+"""
+multiple line comment 
+"""
+
+#source code: its a human understandable language
+#machine code: machine understandble code
+a=2
+print(type(a))
+
+b=("shubham")
+print(type(b))
+
+pi=3.142
+print(type(pi))
+
+#how to take a user input
+name=input("please enter your name")
+print(name)
---
+++ day02/task.py
@@ -0,0 +1,18 @@
+#print the name of the user by taking the name from the user
+
+name=input("please enter your name sir")
+print(name)
+
+
+
+
+#print the first name and last name of the user and print with sep="-"  and end with "\t"
+
+name1=input("please enter your first name")
+name2=input("enter your last name ")
+print(name1,name2,sep="-",end="\t,\n")
+
+#take a user input as a name
+
+name=input("enter your name")
+print("your welcome",name,"to the python class")
